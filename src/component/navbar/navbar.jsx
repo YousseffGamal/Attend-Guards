a2740ece397482c040bd05398c900ff1e5191acb
YousseffGamal <youssefggamal552@gmail.com>
2024-07-16 23:58:26 +0300

    updates

--- src/component/navbar/navbar.jsx
+++ src/component/navbar/navbar.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "./navbar.css";
 import { FaUserClock, FaMapMarkerAlt, FaUserPlus } from 'react-icons/fa';
+import { Link } from "react-router-dom";
 
 const Navbar = ({ activeIcon }) => {
   return (
@@ -11,9 +12,15 @@ const Navbar = ({ activeIcon }) => {
         <div className="row justify-content-center">
           <div className="col-auto nav">
             <div className="images-container">
-              <FaUserClock className={`nav-image ${activeIcon === 'clock' ? 'active-icon' : ''}`} />
-              <FaMapMarkerAlt className={`nav-image ${activeIcon === 'map' ? 'active-icon' : ''}`} />
-              <FaUserPlus className={`nav-image ${activeIcon === 'plus' ? 'active-icon' : ''}`} />
+            <Link to="/DailyRecords">
+            <FaUserClock className={`nav-image ${activeIcon === 'clock' ? 'active-icon' : ''}`} />
+            </Link>
+            <Link to="/SetLocation">
+            <FaMapMarkerAlt className={`nav-image ${activeIcon === 'map' ? 'active-icon' : ''}`} />
+            </Link> 
+            <Link to="/Adduser">
+            <FaUserPlus className={`nav-image ${activeIcon === 'plus' ? 'active-icon' : ''}`} />
+            </Link>
             </div>
           </div>
         </div>
